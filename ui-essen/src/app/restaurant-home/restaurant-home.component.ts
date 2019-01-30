6c30bbd4ad1b7596e41e20b98a71db331fa3f969
Shardulsonar <shardulsonar@gmail.com>
2019-01-30 01:15:12 +0100

    Adding Features for restaurant home

--- ui-essen/src/app/restaurant-home/restaurant-home.component.ts
+++ ui-essen/src/app/restaurant-home/restaurant-home.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RestaurantService } from '../restaurant.service';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { RestaurantModel } from '../restaurant.model'
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatDialog } from '@angular/material/';
+import { RestaurantEditComponent } from '../restaurant-edit/restaurant-edit.component'
+import { RestaurantAddComponent } from '../restaurant-add/restaurant-add.component'
 
 @Component({
   selector: 'app-restaurant-home',
@@ -7,12 +14,27 @@ import { RestaurantService } from '../restaurant.service';
   styleUrls: ['./restaurant-home.component.css', './restaurant-home.component.scss']
 })
 export class RestaurantHomeComponent implements OnInit {
+  public showSearchBar = true;
+  public searchResultsVisible = false;
+  restaurants: Array<RestaurantModel>;
+  public locations: Array<String>;
 
   constructor(
-    private restaurantService: RestaurantService
-  ) { }
-  locations: Array<String>;
-  selectedLocation : String;
+    private searchFormBuilder: FormBuilder,
+    public dialog: MatDialog,
+    private restaurantService: RestaurantService,
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
+    iconRegistry.
+        addSvgIcon('edit',sanitizer.bypassSecurityTrustResourceUrl('assets/baseline-edit-24px.svg')).
+        addSvgIcon('add', sanitizer.bypassSecurityTrustResourceUrl('assets/round-add-24px.svg'))
+  }
+
+  searchForm = this.searchFormBuilder.group({
+    selectedLocation: new FormControl('', Validators.required),
+    searchString: new FormControl('', Validators.required),
+  });
 
   ngOnInit() {
     this.restaurantService.getRestaurantLocaions().subscribe((data: String[]) => {
@@ -21,7 +43,41 @@ export class RestaurantHomeComponent implements OnInit {
   }
 
   onClickSearch() {
-    console.log(this.selectedLocation);
+    this.showSearchBar = false;
+    this.searchResultsVisible = true; // If 200 Response the true or false 
+    this.restaurantService.searchRestaurants(this.searchForm.value.searchString, this.searchForm.value.selectedLocation).subscribe((data: RestaurantModel[]) => {
+      this.restaurants = data;
+      console.log(this.restaurants)
+    });
+    console.log("on click search !!!!!!")
+    
+  }
+  
+  openDialogEditRestaurant(restaurant: RestaurantModel): void {
+    let demo : RestaurantModel = Object.create(restaurant);
+    const dialogRef = this.dialog.open(RestaurantEditComponent, {
+      width: '250px',
+      data: { demo },
+      hasBackdrop: true,
+
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 
+  openDialogAddRestaurant(restaurant: RestaurantModel): void {
+
+    const dialogRef = this.dialog.open(RestaurantAddComponent, {
+      width: '600px',
+      data: { restaurant },
+      hasBackdrop: true,
+
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
